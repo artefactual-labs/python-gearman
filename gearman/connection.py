b68efc868c7a494dd6a2d2e820fb098a6da9f797
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-07-16 10:22:26 -0600

    Add array_from_bytes compatibility helper
    The `array.fromstring` method was deprecated in Python 3.2 and has
    been removed in Python 3.9. This adds a compatibility helper for
    Python 2 and 3.

--- gearman/connection.py
+++ gearman/connection.py
@@ -198,7 +198,7 @@ class GearmanConnection(object):
                 recv_buffer += self.gearman_socket.recv(remaining)
                 remaining = self.gearman_socket.pending()
 
-        self._incoming_buffer.fromstring(recv_buffer)
+        compat.array_from_bytes(self._incoming_buffer, recv_buffer)
         return len(self._incoming_buffer)
 
     def _unpack_command(self, given_buffer):
